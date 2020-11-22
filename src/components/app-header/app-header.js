1f9682a783cd06ce3430619332d9e04f23ff70f0
Алексей <tov.kessler@gmail.com>
2020-11-22 20:24:34 +0800

    Starting setup

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./app-header.scss";
+
+function AppHeader() {
+    return (
+        <div>
+            LOGO
+        </div>
+    );
+}
+
+export default AppHeader;
---
+++ src/components/app/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.scss';
+
+
+import AppHeader from '../app-header';
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader></AppHeader>
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
